bafc5f8d7c8f31f16e2f5cd817d1cf56fe841deb
Jahanzeb Malik <jahanxeb.m@gmail.com>
2019-06-17 23:25:55 +0500

    man website banner creation

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,4 +2,16 @@ $(document).ready(function () {
     $('.sidenav').sidenav();
 });
 
-$(".button-collapse").sideNav();
+// $(".button-collapse").sideNav();
+
+// var instance = M.Carousel.init({
+//     fullWidth: true,
+//     indicators: true,
+//     duration: 200
+// });
+
+$(document).ready(function () {
+    $(".carousel").carousel()
+});
+
+M.AutoInit();
